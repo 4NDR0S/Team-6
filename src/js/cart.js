f57444d1f1ca1132791f41f659d5c51868950eff
roj21008 <roj21008@byui.edu>
2024-01-13 18:05:57 -0500

    BRANCH FIXED

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,35 @@
 import { getLocalStorage } from "./utils.mjs";
-
+// export { cartItems };
+ 
+// const carrito = window.document.querySelector(".cart");
+// const pruebita = window.document.querySelector(".product-list");
+ 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+ 
+ 
+  //mi contador------
+  // const cartItems = [];
+  // cartItems.push(getLocalStorage("so-cart"));
+ 
+  // const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+ 
+  // document.querySelector(".product-list").innerHTML = htmlItems.join("");
+ 
+  // let contador = 0;
+  // for (let i = 0; i < cartItems.length; i++) {
+  //   contador++;
+  // }
+ 
+  // // const carrito = document.querySelector(".cart");
+  // let numero = window.document.createElement("p");
+  // numero.textContent = `${contador} item added`;
+  // carrito.appendChild(numero);
+ 
 }
-
+ 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -21,8 +45,9 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
+ 
   return newItem;
 }
-
+ 
 renderCartContents();
+ 
